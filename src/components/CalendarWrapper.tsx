4c1cc62389fba95d53bbcb7e8c95f65f117eb6b8
Mateusz Obirek <neutralnie7@gmail.com>
2025-10-22 13:07:08 +0200

    edit

--- src/components/CalendarWrapper.tsx
+++ src/components/CalendarWrapper.tsx
@@ -1,17 +1,14 @@
 "use client";
 import React, { useState } from "react";
-import {
-  EventCalendar,
-  // type CalendarEvent
-} from "@/components/event-calendar";
+import { EventCalendar, type CalendarEvent } from "@/components/event-calendar";
 const CalendarWrapper = () => {
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState([] as CalendarEvent[]);
 
-  const handleEventAdd = (event) => {
-    setEvents([...events, event]);
+  const handleEventAdd = (event: CalendarEvent): void => {
+    setEvents([...events, event] as never);
   };
 
-  const handleEventUpdate = (updatedEvent) => {
+  const handleEventUpdate = (updatedEvent: CalendarEvent): void => {
     setEvents(
       events.map((event) =>
         event.id === updatedEvent.id ? updatedEvent : event
@@ -19,8 +16,8 @@ const CalendarWrapper = () => {
     );
   };
 
-  const handleEventDelete = (eventId) => {
-    setEvents(events.filter((event) => event.id !== eventId));
+  const handleEventDelete = (eventId: string): void => {
+    setEvents(events.filter((event: CalendarEvent) => event.id !== eventId));
   };
 
   return (
